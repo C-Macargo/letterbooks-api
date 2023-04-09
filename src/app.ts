17a97d23463bb7902f35e93e44a016dcd4c6ed60
C-Macargo <caio.camargos97@gmail.com>
2023-04-09 17:05:32 -0300

    feat, setup server

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { handleApplicationErrors } from "middlewares/erro.middleware";
+import router from "./routes/app.routes";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(router);
+app.use(handleApplicationErrors);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port : ${port}`));
---
+++ src/routes/user.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const userRoutes = Router();
+
+userRoutes.post('/signup');
+userRoutes.post('/signin',);
+
+
+export default userRoutes;
